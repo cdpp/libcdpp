af4f4e72748c1bf80171c16351a3ce7b0987eb6c
CDPP <cdpp@users.noreply.github.com>
2015-01-27 02:08:26 +0100

    [DLC] Fully implemented -> released version 0.1.3a

--- version.h
+++ version.h
@@ -4,7 +4,11 @@
 namespace AutoVersion{
 	
 	//Date Version Types
+<<<<<<< HEAD
 	#define DATE "29"
+=======
+	#define DATE "27"
+>>>>>>> [DLC] Fully implemented -> released version 0.1.3a
 	#define MONTH "01"
 	#define YEAR "2015"
 	#define UBUNTU_VERSION_STYLE  "15.01"
@@ -16,6 +20,7 @@ namespace AutoVersion{
 	//Standard Version Type
 	#define MAJOR  0
 	#define MINOR  1
+<<<<<<< HEAD
 	#define BUILD  4
 	#define REVISION  3
 	
@@ -27,6 +32,19 @@ namespace AutoVersion{
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	#define BUILD_HISTORY  1
+=======
+	#define BUILD  3
+	#define REVISION  1
+	
+	//Miscellaneous Version Types
+	#define BUILDS_COUNT  82
+	#define RC_FILEVERSION 0,1,3,1
+	#define RC_FILEVERSION_STRING "0, 1, 3, 1\0"
+	#define FULLVERSION_STRING  "0.1.3.1"
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	#define BUILD_HISTORY  4
+>>>>>>> [DLC] Fully implemented -> released version 0.1.3a
 	
 
 }
